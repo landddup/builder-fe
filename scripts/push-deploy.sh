8b8713e9fce03278f6b7b443e56f9c199a516b95
Maksym Pokhylyi <maksimpooh@gmail.com>
2023-04-14 10:31:49 +0300

    edit script

--- scripts/push-deploy.sh
+++ scripts/push-deploy.sh
@@ -3,10 +3,18 @@ echo "Enter commit message"
 read commit
 echo "Enter target branch"
 read branch
+echo "Is branch already exist? y/n"
+read checkout
+
+if [$checkout="n"]
+then
+    git checkout -b $branch
+else
+    git checkout $branch
+fi
+
 git add .
 git commit -m "$commit"
-git checkout $branch
-git pull origin $branch
 git push origin $branch
 npm run build
 firebase deploy
