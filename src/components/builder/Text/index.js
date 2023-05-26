2971555dc1706e9d1d7f913d63617539dea7d652
Maksym Pokhylyi <maksimpooh@gmail.com>
2023-05-26 21:18:51 +0300

    rm text editing

--- src/components/builder/Text/index.js
+++ src/components/builder/Text/index.js
@@ -1,50 +1,15 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
-
-import { Input } from "../../shared";
 
 import styles from "./index.module.scss";
 
 const Text = ({ currentNode }) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [input, setInput] = useState(currentNode.content);
-
   const Component = useMemo(
     () => currentNode.component,
     [currentNode.component]
   );
 
-  const handleBeginEdit = () => {
-    setIsEditing(true);
-  };
-
-  const handleInputChange = (value) => {
-    setInput(value);
-  };
-
-  const handleSubmit = () => {
-    setIsEditing(false);
-  };
-
-  if (isEditing) {
-    return (
-      <Input
-        className={styles.text}
-        value={input}
-        onChange={handleInputChange}
-        onBlur={handleSubmit}
-        labelAllowed={false}
-        errorMessageAllowed={false}
-        autoFocus
-      />
-    );
-  }
-
-  return (
-    <Component className={styles.text} onClick={handleBeginEdit}>
-      {input}
-    </Component>
-  );
+  return <Component className={styles.text}>{currentNode.content}</Component>;
 };
 
 Text.propTypes = {
